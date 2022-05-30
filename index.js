98d2674f1145aa38399d7fc6e9ee5a91781eec9e
joaoCamposSC <joao.campos@sharecare.com>
2022-05-30 13:43:20 -0400

    add /file endpoint

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const {parse} = require('csv-parse/sync')
+
+const MAX_FILE_SIZE = 15 * 1024 * 1024
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage, limits: {fieldSize: MAX_FILE_SIZE}})
+
+const app = express()
+
+const port = 3001
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send({"data": "hello world"})
+})
+
+app.post('/file', upload.single('file'), async (req, res) => {
+    const {buffer} = req.file || {}
+    const json = parse(buffer.toString(), {
+        delimiter: ',',
+        from: 2,
+        trim: true,
+        columns: true
+    })
+    console.log(json)
+    res.send(json)
+})
+
+const startServer = () => {
+
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    })
+}
+
+startServer()
