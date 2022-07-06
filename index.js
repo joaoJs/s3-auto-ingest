38af0e54f12949406d63a0ed43fe432456792b15
joaoCamposSC <joao.campos@sharecare.com>
2022-07-06 08:57:50 +0200

    add async

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { parse } = require('csv-parse/sync')
-import fetch from 'node-fetch'
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express()
 
